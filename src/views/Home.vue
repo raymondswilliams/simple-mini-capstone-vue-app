619ebf69f084dced0cd7c4c202ea334ff36c66b5
shane <willyums86@gmail.com>
2021-10-05 15:02:16 -0400

    Updates APP

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,7 @@
     <div>
       Image_url: <input type="text" v-model="newProductParams.image_url" />
     </div>
+    <p>newProductParams: {{ newProductParams }}</p>
     <button v-on:click="createProduct()">Create</button>
     <h1>All Products</h1>
     <div v-for="product in products" v-bind:key="product.id">
@@ -16,7 +17,22 @@
       <img :src="product.image_url" alt="" />
       <p>Price: {{ product.price }}</p>
       <p>Description: {{ product.description }}</p>
+      <button v-on:click="showProduct(product)">See Info</button>
     </div>
+    <dialog id="product-details">
+      <form method="dialog">
+        <h1>Product Details</h1>
+        currentProduct: {{ currentProduct }}
+        <p>Name: <input type="text" v-model="currentProduct.name" />/p></p>
+        <p>Price: <input type="text" v-model="currentProduct.price" /></p>
+        <p>
+          Description:
+          <input type="text" v-model="currentProduct.description" />
+        </p>
+        Image Url: <input type="text" v-model="currentProduct.image_url" />
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
 </template>
 
@@ -24,12 +40,14 @@
 
 <script>
 import axios from "axios";
+import func from "vue-editor-bridge";
 // import func from "vue-editor-bridge";
 export default {
   data: function () {
     return {
       products: [],
       newProductParams: {},
+      currentProduct: [],
     };
   },
   created: function () {
@@ -43,17 +61,43 @@ export default {
       });
     },
     createProduct: function () {
-      var params = {
-        name: "Water Bottle",
-        price: "10",
-        description: "Store your preferred H20 in this hand wash only product!",
-        image_url: "",
-      };
-
-      axios.post("http://localhost:3000/products", params).then((response) => {
-        console.log(response.data);
-        this.products.push(response.data);
-      });
+      axios
+        .post("http://localhost:3000/products", this.newProductarams)
+        // happy status code/path
+        .then((response) => {
+          console.log(response.data);
+          this.products.push(response.data);
+        })
+        // sad status code/path
+        .catch((error) => {
+          console.log(error.response.data.errors);
+        });
+    },
+    showProduct: function (product) {
+      console.log(product);
+      this.currentProduct = product;
+      document.querySelector("#product-details").showModal();
+    },
+    updateProduct: function (product) {
+      var editProductParams = product;
+      axios
+        .patch(
+          `http://localhost:3000/products/${product.id}`,
+          editProductParams
+        )
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+        });
+    },
+    destroyProduct: function (product) {
+      axios
+        .delete(`http://localhost:3000/products/${product.id}`)
+        .then((response) => {
+          console.log("Killed it,", response.data);
+        });
     },
   },
 };
