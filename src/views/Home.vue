788236e2f7c89a116a3883bcb89f4698facb3307
shane <willyums86@gmail.com>
2021-10-05 12:13:54 -0400

    New Vue App

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>New Product</h1>
+    <div>Name: <input type="text" v-model="newProductParams.name" /></div>
+    <div>Price: <input type="text" v-model="newProductParams.price" /></div>
+    <div>
+      Description: <input type="text" v-model="newProductParams.description" />
+    </div>
+    <div>
+      Image_url: <input type="text" v-model="newProductParams.image_url" />
+    </div>
+    <button v-on:click="createProduct()">Create</button>
+    <h1>All Products</h1>
+    <div v-for="product in products" v-bind:key="product.id">
+      <h2>Name: {{ product.name }}</h2>
+      <img :src="product.image_url" alt="" />
+      <p>Price: {{ product.price }}</p>
+      <p>Description: {{ product.description }}</p>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style></style>
 
+<script>
+import axios from "axios";
+// import func from "vue-editor-bridge";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      products: [],
+      newProductParams: {},
+    };
+  },
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("http://localhost:3000/products").then((response) => {
+        console.log(response.data);
+        this.products = response.data;
+      });
+    },
+    createProduct: function () {
+      var params = {
+        name: "Water Bottle",
+        price: "10",
+        description: "Store your preferred H20 in this hand wash only product!",
+        image_url: "",
+      };
+
+      axios.post("http://localhost:3000/products", params).then((response) => {
+        console.log(response.data);
+        this.products.push(response.data);
+      });
+    },
   },
 };
 </script>
